2508ab6d55ea94b2833507025a1a2672eddf4534
ChamodiLiyanage <it21828966@my.sliit.lk>
2024-08-31 15:47:04 +0530

    Interface

---
+++ devspark_spm_research_project/devspark_core_model/ForumPortalModels/Answer.cs
@@ -0,0 +1,24 @@
+﻿using devspark_core_model.SystemModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devspark_core_model.ForumPortalModels
+{
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public int QuestionId { get; set; }
+
+        [DisplayName("Explanation")]
+        public string Explanation { get; set; }
+        public string UserId { get; set; }
+        public DateTime DatePosted { get; set; }
+        public Question Question { get; set; }
+        public User User { get; set; }
+         
+    }
+}
