37fb697eb5a37ddcd90ba4d9096e8cdd0da0ba66
Jehan16 <jehanshenil16@gmail.com>
2024-09-26 19:29:28 +0530

    Code snippet feature

---
+++ devspark_spm_research_project/devspark_core_web/Areas/ContributionPortal/Controllers/CodeSnippetController.cs
@@ -0,0 +1,44 @@
+﻿using devspark_core_model.ContributionPortalModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace devspark_core_web.Areas.ContributionPortal.Controllers
+{
+    public class CodeSnippetController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CodeSnippet codesnippet)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CodeSnippet codesnippet)
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(CodeSnippet codesnippet)
+        {
+            return View();
+        }
+    }
+}
